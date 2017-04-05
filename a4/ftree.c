cea6d4125b9ac15da6c856aacb459ad148ad4ea7
Chen Zhang <sangria.zhang@mail.utoronto.ca>
2017-04-05 11:12:13 -0400

    merge server and client codes in ftree.c

--- a4/ftree.c
+++ a4/ftree.c
@@ -45,6 +45,15 @@ void sendfile(char *source, char *host, unsigned short port, struct request *inf
 void load_hash_request(char *hash_val, struct request *req_ptr);
 int rcopy_client_body(char *source, char* host, unsigned short port, int sock_fd);
 
+int bindandlisten(void);
+static struct client *addclient(struct client *top, int fd, struct in_addr addr);
+static struct client *removeclient(struct client *top, int fd);
+int handleclient(struct client *p, struct client *top);
+void check_REGFILE(struct client *p);
+int need_send_file(struct client *p);
+void check_REGDIR(struct client *p);
+int process_data(struct client *p);
+
 int rcopy_client(char *source, char *host, unsigned short port) {
 	// Create the socket FD.
 	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -363,40 +372,6 @@ char *generate_path(char *fname, char *c_name) {
 	return path;
 }
 
-
-/*
- * Client writes a single string buf to correponding socket descriptor.
- * Note: this write includes writing the null terminator for a string
- */
-void client_write_str(int sock_fd, char *buf) {
-	int should_write = strlen(buf) + 1;
-	int num_written = write(sock_fd, buf, should_write);
-	if (num_written != should_write) {
-		perror("client: write");
-		close(sock_fd);
-		exit(1);
-	}
-}
-
-/*
- * Return a path for the root file/ or dir to start copy in the server side, 
- * based on the basename read from client.
- */
-char *server_generate_copy_root(int sock_fd) {
-	char basename[BUF_SIZE + 1];
-	fprintf(stderr, "read basename not start");
-	int num_read = read(sock_fd, &basename, BUF_SIZE);
-	fprintf(stderr, "read basename: %s\n", basename);
-	if (num_read == 0) {
-		perror("server: read");
-		close(sock_fd);
-		exit(1);
-	}
-	char *path = generate_path(CURRENT_WORKING_DIR, basename);
-	fprintf(stderr, "path in sgcr: %s\n", path);
-	return path;
-}
-
 /*
  * Compute the hash for file at path fname with correponding request 
  * at req_ptr.
@@ -479,5 +454,440 @@ void client_write_fields(int sock_fd, struct request *req_ptr) {
 		perror("client: write size");
 		close(sock_fd);
 		exit(1);
+	}
+}
+
+void rcopy_server(unsigned short port) {
+    int clientfd, maxfd, nready;
+    struct client *p;
+    struct client *head = NULL;
+    socklen_t len;
+    struct sockaddr_in q;
+    struct timeval tv;
+    fd_set allset;
+    fd_set rset;
+
+    int i;
+
+    int listenfd = bindandlisten();
+    // initialize allset and add listenfd to the
+    // set of file descriptors passed into select
+    FD_ZERO(&allset);
+    FD_SET(listenfd, &allset);
+    // maxfd identifies how far into the set to search
+    maxfd = listenfd;
+
+    while (1) {
+        // make a copy of the set before we pass it into select
+        rset = allset;
+        /* timeout in seconds (You may not need to use a timeout for
+        * your assignment)*/
+        tv.tv_sec = 10;
+        tv.tv_usec = 0;  /* and microseconds */
+
+        nready = select(maxfd + 1, &rset, NULL, NULL, &tv);
+        if (nready == 0) {
+            printf("No response from clients in %ld seconds\n", tv.tv_sec);
+            continue;
+        }
+        printf("Time says %ld seconds\n", tv.tv_sec);
+
+        if (nready == -1) {
+            perror("select");
+            continue;
+        }
+
+        if (FD_ISSET(listenfd, &rset)){
+            printf("a new client is connecting\n");
+            len = sizeof(q);
+            if ((clientfd = accept(listenfd, (struct sockaddr *)&q, &len)) < 0) {
+                perror("accept");
+                exit(1);
+            }
+            FD_SET(clientfd, &allset);
+            if (clientfd > maxfd) {
+                maxfd = clientfd;
+            }
+            printf("connection from %s\n", inet_ntoa(q.sin_addr));
+            head = addclient(head, clientfd, q.sin_addr);
+        }
+
+        for(i = 0; i <= maxfd; i++) {
+            if (FD_ISSET(i, &rset)) {
+                for (p = head; p != NULL; p = p->next) {
+                    if (p->fd == i) {
+                        int result = handleclient(p, head);
+                        if (result == -1) {
+                            int tmp_fd = p->fd;
+                            head = removeclient(head, p->fd);
+                            FD_CLR(tmp_fd, &allset);
+                            close(tmp_fd);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+ /* bind and listen, abort on error
+  * returns FD of listening socket
+  */
+int bindandlisten(void) {
+    struct sockaddr_in r;
+    int listenfd;
+
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket");
+        exit(1);
+    }
+    int yes = 1;
+    if ((setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))) == -1) {
+        perror("setsockopt");
+    }
+    memset(&r, '\0', sizeof(r));
+    r.sin_family = AF_INET;
+    r.sin_addr.s_addr = INADDR_ANY;
+    r.sin_port = htons(PORT);
+
+    if (bind(listenfd, (struct sockaddr *)&r, sizeof r)) {
+        perror("bind");
+        exit(1);
+    }
+
+    if (listen(listenfd, 5)) {
+        perror("listen");
+        exit(1);
+    }
+    return listenfd;
+}
+
+static struct client *addclient(struct client *top, int fd, struct in_addr addr) {
+    struct client *p = malloc(sizeof(struct client));
+    if (!p) {
+        perror("malloc");
+        exit(1);
+    }
+
+    printf("Adding client %s\n", inet_ntoa(addr));
+
+    p->fd = fd;
+    p->ipaddr = addr;
+    p->next = top;
+    top = p;
+    return top;
+}
+
+static struct client *removeclient(struct client *top, int fd) {
+    struct client **p;
+
+    for (p = &top; *p && (*p)->fd != fd; p = &(*p)->next)
+        ;
+    // Now, p points to (1) top, or (2) a pointer to another client
+    // This avoids a special case for removing the head of the list
+    if (*p) {
+        struct client *t = (*p)->next;
+        printf("Removing client %d %s\n", fd, inet_ntoa((*p)->ipaddr));
+        free(*p);
+        *p = t;
+    } else {
+        fprintf(stderr, "Trying to remove fd %d, but I don't know about it\n",
+                 fd);
+    }
+    return top;
+}
+
+/* Accept a connection. Note that a new file descriptor is created for
+ * communication with the client. The initial socket descriptor is used
+ * to accept connections, but the new socket is used to communicate.
+ * Return the new client's file descriptor.
+ */
+int accept_connection(int fd) {
+	int client_fd = accept(fd, NULL, NULL);
+	if (client_fd < 0) {
+		perror("server: accept");
+		close(fd);
+		exit(1);
+	}
+	return client_fd;
+}
+
+/*
+ * Extract the basename part in the path fname and return that name.
+ * Note: reuse part of my A3 helper function extract_name
+ *
+ * Precondition: fname is a valid path
+ */
+char *extract_name(char *fname) {
+	
+	// If fname does not have slash, itself is already a basename
+	char *first_occur = strchr(fname, '/');
+	if (first_occur == NULL) {
+		return str_copy(fname);
+	}
+	
+	// Deal with the path that has slash(es)
+	char *result = NULL;
+	char *temp = str_copy(fname);
+	
+	// Get rid of trailing slash(es) except when it occurs also as the
+	// first character in the path
+	int j = strlen(fname) - 1;
+	while (j > 0 && temp[j] == '/') {
+		temp[j] = '\0';
+		j--;
+	}
+	
+	// Mark the start of the basename part in path temp
+	char *name_start = strrchr(temp, '/');
+	if (name_start == NULL) {
+		name_start = temp;
+	} else if (name_start[1] != '\0') {
+		name_start += 1;
+	}
+	result = str_copy(name_start);
+	
+	// Free memory of no further use and return result
+	free(temp);
+	return result;
+}
+
+/*
+ * Return a copy of given source string src.
+ * Note: reuse part of my A3 helper function str_copy
+ */
+char *str_copy(char* src) {
+	int length = strlen(src);
+	char *result = malloc(sizeof(char) * (length + 1));
+	memcpy(result, src, length);
+	result[length] = '\0';
+	return result;
+}
+
+/*
+ * Return the path for a child directory or file using the path of its parent
+ * directory and the name of that child directory/ file.
+ *
+ * Precondition: fname is a valid path representing a directory
+ * Note: reuse A3 generate_path function
+ */
+char *generate_path(char *fname, char *c_name) {
+	
+	// Allocate memory for path
+	int p_size = sizeof(char) * (strlen(fname) + strlen(c_name) + 2);
+	char *path = malloc(p_size);
+	
+	// Make path a string so that we can use string operations
+	path[0] = '\0';
+	
+	// We have allocated enough memory, so we don't need
+	// to use strncpy and strncat here
+	// And we also deal with the trailing slashes for path fname
+	strcpy(path, fname);
+	int i = strlen(fname) - 1;
+	while (i > 0 && path[i] == '/') {
+		path[i] = '\0';
+		i--;
+	}
+	
+	// If parent dir is root directory for the OS, i.e. now what is already
+	// concatenated to the path is a single /,
+	// then we don't need to concatenate slash any more
+	if (path[strlen(path) - 1] != '/') {
+		strcat(path, "/");
+	}
+	
+	// strcat ensures null terminator and we allocated enough memory
+	strcat(path, c_name);
+	
+	// Return the path
+	return path;
+}
+
+
+/*
+ * Client writes a single string buf to correponding socket descriptor.
+ * Note: this write includes writing the null terminator for a string
+ */
+void client_write_str(int sock_fd, char *buf) {
+	int should_write = strlen(buf) + 1;
+	int num_written = write(sock_fd, buf, should_write);
+	if (num_written != should_write) {
+		perror("client: write");
+		close(sock_fd);
+		exit(1);
+	}
+}
+
+/*
+ * Return a path for the root file/ or dir to start copy in the server side, 
+ * based on the basename read from client.
+ */
+char *server_generate_copy_root(int sock_fd) {
+	char basename[BUF_SIZE + 1];
+	fprintf(stderr, "read basename not start");
+	int num_read = read(sock_fd, &basename, BUF_SIZE);
+	fprintf(stderr, "read basename: %s\n", basename);
+	if (num_read == 0) {
+		perror("server: read");
+		close(sock_fd);
+		exit(1);
+	}
+	char *path = generate_path(CURRENT_WORKING_DIR, basename);
+	fprintf(stderr, "path in sgcr: %s\n", path);
+	return path;
+}
+
+/*
+ * Server loads all fields.
+ */
+int handleclient(struct client *p, struct client *top) {
+	switch (p->status) {
+	case AWAITING_TYPE: {
+		if (read(p->fd, &(p->req.type), sizeof(int)) != sizeof(int)){
+			perror("server: load type");
+			return -1;
+		}
+		p->req.type = ntohl(p->req.type);
+		p->status = AWAITING_PATH;
+	} break;
+	case AWAITING_PATH: {
+		if (read(p->fd, p->req.path, MAXPATH) != MAXPATH){
+			perror("server: load path");
+			return -1;
+		}
+		p->status = AWAITING_PERM;
+	} break;
+	case AWAITING_PERM: {
+		if (read(p->fd, &p->req.mode, sizeof(mode_t)) != sizeof(mode_t)){
+			perror("server: load mode");
+			return -1;
+		}
+		p->status = AWAITING_HASH;
+	} break;
+	case AWAITING_HASH: {
+		if (read(p->fd, p->req.hash, BLOCKSIZE) != BLOCKSIZE){
+			perror("server: load hash");
+			return -1;
+		}
+		p->status = AWAITING_SIZE;
+	} break;
+	case AWAITING_SIZE: {
+		if (read(p->fd, &p->req.size, sizeof(int)) != sizeof(int)){
+			perror("rcopy_server: read client SIZE");
+			return -1;
+		}
+		p->req.size = ntohl(p->req.size);
+		switch (p->req.type) {
+		case REGFILE: {
+			check_REGFILE(p);
+			p->status = AWAITING_TYPE;
+		} break;
+		case REGDIR: {
+			check_REGDIR(p);
+			p->status = AWAITING_TYPE;
+		} break;
+		case TRANSFILE: {
+// 			handleclient_tfile(p);
+			p->status = AWAITING_DATA;
+		} break;
+		}
+	} break;
+	case AWAITING_DATA: {
+		return process_data(p);
+	} default: // should not get here
+		break;
+	}
+	return 0;
+}
+
+void check_REGFILE(struct client *p) {
+	int response;
+	int result;
+	if ((result = need_send_file(p)) == 0) { // need
+		reponse = SENDFILE;
+	} else if (result == 1){ // OK
+		response = OK;
+	} else {
+		response = ERROR;
+	}
+	
+	// Send response to client
+	uint32_t net_uint = htonl(response);
+	int num_written = write(p->fd, &net_uint, sizeof(uint32_t));
+	if (num_written != sizeof(uint32_t)) {
+		perror("server: write response");
+		close(p->fd);
+		exit(1);
+	}
+}
+
+/*
+ * 0 for need, 1 for not, -1 for error
+ */
+int need_send_file(struct client *p) {
+	
+	// Check existence
+	struct stat local_stat;
+	int exist = lstat(p->path, &local_stat);
+	if (exist < 0) {
+		 return 0;
+	}
+	
+	// Check whether incompatible type
+	if ((S_ISREG(local_stat.st_mode) && p->type == REGDIR)
+		|| (S_ISDIR(local_stat.st_mode) && p->type == REGFILE)) {
+		fprintf(stderr, "server: find incompatible type");
+		return -1;
+	}
+	
+	// Already exists, check whether same
+	// compare size
+	if (local_stat.st_size != p->size) {
+		return 0;
+	}
+	
+	// compare hash
+	char *local_hash = compute_hash(p->path);
+	if ((check_hash(local_hash, p->hash)) == 1) {
+		return 0;
+	}
+	
+	// Exist and same, no need to copy
+	return 1;
+}
+
+void check_REGDIR(struct client *p) {
+	
+	// Check existence
+	DIR *dirp;
+	struct stat local_stat;
+	int exist = lstat(p->path, &local_stat);
+	if (exist >= 0) {
+		return;
+	}
+	
+	if ((mkdir(p->path, p->mode)) != 0) {
+		perror("server: mkdir");
+		exit(1);
+	}
+}
+
+int process_data(struct client *p) {
+	if (p->size == 0) {
+		return 0;
+	}
+	
+	char buf[MAX_DATA];
+	int num_read;
+	if ((num_read = read(p->fd, buf, MAX_DATA)) <= 0) {
+		perror("server: process data");
+		return -1;
+	}
+
+	if ((fwrite(buf, sizeof(char), num_read, p->fp)) != num_read) {
+		perror("fwrite");
+		return -1;
 	}
 }
